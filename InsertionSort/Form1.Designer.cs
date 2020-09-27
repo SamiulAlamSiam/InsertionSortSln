1db1325bd60a840ab25c6d500a70f2450405be71
samiul1111 <samiul1111@gmail.com>
2020-09-28 01:10:37 +0600

    first commit

---
+++ InsertionSort/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace InsertionSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblArrayItem = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.92857F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.07143F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1400, 609);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnStart);
+            this.panel2.Controls.Add(this.lblArrayItem);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(1066, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(331, 603);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(63, 460);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(200, 42);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblArrayItem
+            // 
+            this.lblArrayItem.FormattingEnabled = true;
+            this.lblArrayItem.ItemHeight = 16;
+            this.lblArrayItem.Items.AddRange(new object[] {
+            "15",
+            "10",
+            "5",
+            "20",
+            "25",
+            "1"});
+            this.lblArrayItem.Location = new System.Drawing.Point(44, 20);
+            this.lblArrayItem.Name = "lblArrayItem";
+            this.lblArrayItem.Size = new System.Drawing.Size(219, 340);
+            this.lblArrayItem.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1057, 603);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnNext.Location = new System.Drawing.Point(0, 559);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(1055, 42);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 609);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lblArrayItem;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ InsertionSort/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InsertionSort
+{
+    public enum StepsEnum
+    {
+        Start = 0,
+        Selection = 1,
+        Comparison = 2,
+        Success = 3,
+        Failure = 4
+    }
+
+    public partial class Form1 : Form
+    {
+        private int w = 15, l = 185;
+        private int i = 0, j = 0;
+
+        int sortedIndex=0;
+        int index = 0;
+        private int count = 0;
+        private int t = 0;
+        private int prevX = 0;
+
+        List<Button> allBtns = new List<Button>();
+        List<Button> sortedBtns = new List<Button>();
+        List<Button> reverseBtns = new List<Button>();
+        List<int> locationX = new List<int>();
+        List<int> locationY = new List<int>(); 
+        private bool test = true;
+
+        Button firstButton, secondButton;
+        
+
+        private StepsEnum currentStep;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            currentStep = StepsEnum.Start;
+            i = j = -1;
+            foreach (var item in lblArrayItem.Items)
+            {
+                Button btn = new Button();
+                btn.Text = item.ToString();
+                btn.Location = new Point(w, l);
+                btn.Size = new Size(75, 25);
+                btn.BackColor = Color.DarkCyan;
+                btn.ForeColor = Color.White;
+
+                this.panel1.Controls.Add(btn);
+                allBtns.Add(btn);
+                //sortedBtns.Add(btn);
+
+                w += 85;
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            for (int k = 0; k < allBtns.Count; k++)
+            {
+                allBtns[k].BackColor = Color.DarkCyan;
+            }
+
+            if (currentStep == StepsEnum.Start)
+                Start();
+            else if (currentStep == StepsEnum.Selection)
+                Selection();
+            else if (currentStep == StepsEnum.Comparison)
+                CompareValue();
+            else if (currentStep == StepsEnum.Failure)
+                Failure();
+            else if (currentStep == StepsEnum.Success)
+                Success();
+        }
+
+        
+
+        private void Success()
+        {
+            if (sortedBtns.Count != 0 && locationX.Count != 0)
+            {
+                sortedBtns.Clear();
+                locationX.Clear();
+                j = 0;
+            }
+           
+            for (int k = 0; k < allBtns.Count; k++)
+            {
+                if (index > sortedIndex)
+                {
+                    firstButton = allBtns[index - 1];
+                    secondButton = allBtns[index];
+
+                    sortedBtns.Add(firstButton);
+
+                    prevX = firstButton.Location.X;
+
+                    Button temp;
+                    temp = allBtns[index];
+                    allBtns[index] = allBtns[index - 1];
+                    allBtns[index - 1] = temp;
+
+                    
+
+                    count = 1;
+                }
+                else
+                {
+                    break;
+                }
+
+                index--;
+            }
+
+            sortedIndex++;
+            currentStep = StepsEnum.Selection;
+
+            sortedBtns.Reverse();
+            sortedBtns.Add(secondButton);
+
+            foreach (var sortedBtn in sortedBtns)
+            {
+                locationX.Add(sortedBtn.Location.X);
+                locationY.Add(sortedBtn.Location.Y);
+            }
+
+            timer1.Start();
+            btnNext.Enabled = false;
+            //Selection();
+        }
+
+        private void Failure()
+        {
+            sortedIndex++;
+            currentStep = StepsEnum.Selection;
+            Selection();
+            allBtns[index].BackColor = Color.DarkRed;
+        }
+
+        private void CompareValue()
+        {
+            if (index == sortedIndex)
+            {
+                currentStep = StepsEnum.Failure;
+                allBtns[index].BackColor = Color.DarkRed;
+            }
+
+            else 
+            {
+                currentStep = StepsEnum.Success;
+                allBtns[index].BackColor = Color.DarkGreen;
+            }
+        }
+
+        private void Selection()
+        {
+            btnNext.Enabled = true;
+            index = sortedIndex;
+
+            if (sortedIndex == allBtns.Count-1)
+            {
+                btnNext.Enabled = false;
+            }
+
+            int min = Int32.Parse(allBtns[sortedIndex].Text);
+
+            
+            for (int i = sortedIndex; i < allBtns.Count; i++)
+            {
+                if (min > Int32.Parse(allBtns[i].Text))
+                {
+                    t = 1;
+                    index = i;
+                    min = Int32.Parse(allBtns[i].Text);
+                }
+                    
+            }
+
+            allBtns[index].BackColor = Color.DarkBlue;
+
+            currentStep = StepsEnum.Comparison;            
+
+            //if(sortedIndex>0)
+            //    allBtns.RemoveAt(sortedIndex-1);
+        }
+
+        private void Start()
+        {
+            
+            currentStep = StepsEnum.Selection;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(count!=2)
+                i = sortedBtns.Count - 2;
+                if (j < 5)
+                {
+                    secondButton.Location = new Point(secondButton.Location.X, secondButton.Location.Y - 5);
+                    j++;
+                    count = 1;
+                }
+
+                else if (i < 0)
+                {
+                    timer1.Stop();
+                    btnNext.Enabled = true;
+                }
+
+                else if (secondButton.Location.X > locationX[0])
+                {
+                    secondButton.Location = new Point(secondButton.Location.X - 5, secondButton.Location.Y);
+                }
+
+                else if (sortedBtns[i].Location.X > locationX[i + 1])
+                {
+                    count = 2;
+                    i--;
+                }
+
+                else if (secondButton.Location.Y < locationY[0] && sortedBtns[0].Location.X > locationX[0] + 84)
+                {
+                    secondButton.Location = new Point(secondButton.Location.X, secondButton.Location.Y + 5);
+                }
+
+                else 
+                {
+                    sortedBtns[i].Location = new Point(sortedBtns[i].Location.X + 5, sortedBtns[i].Location.Y);
+                    count = 1;
+                }     
+        }
+    }
+}
